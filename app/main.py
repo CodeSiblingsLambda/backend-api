424292207e739cf48c7d7aa0e583e4eb7087cced
richardesp <espantaleon2001@gmail.com>
2025-08-06 00:14:24 +0200

    feat: add /auth routers

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 import uvicorn.config
 
+from app.api.v1.auth import router as auth_router
+
 # Initialize FastAPI
 app = FastAPI()
 
+# Include routes
+app.include_router(auth_router)
+
 if __name__ == "__main__":
     import uvicorn
+
     print("Hello world")
 
     uvicorn.run(app)
