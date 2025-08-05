bc373e151474820f65361af5bd9fbd0c2175be3d
richardesp <espantaleon2001@gmail.com>
2025-08-06 00:14:42 +0200

    refactor: adjust post methods for login and signup

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,25 +1,34 @@
 from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.responses import JSONResponse
+
 from app.schemas.auth import CustomerLogin, CustomerResponse, CustomerSignUp
 from app.services.auth import login_user, signup_client
+from app.database.get_db import get_db
+from app.utils.logger import create_logger
 
+logger = create_logger(__name__)
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @router.post("/login", response_model=CustomerResponse, status_code=status.HTTP_200_OK)
-def login(data: CustomerLogin) -> CustomerResponse:
+async def login(
+    data: CustomerLogin, db: AsyncSession = Depends(get_db)
+) -> CustomerResponse:
     """
     Authenticate a customer and return their profile (excluding password).
     """
-    return login_user(data)
+    logger.info(f"Calling login_user with the following data: {data}")
+    return await login_user(data, db)
 
 
 @router.post("/signup/client", status_code=status.HTTP_201_CREATED)
-def signup_client_endpoint(data: CustomerSignUp) -> JSONResponse:
+async def signup_client_endpoint(data: CustomerSignUp) -> JSONResponse:
     """
     Register a new customer. Returns success message if created.
     """
-    signup_client(data)
+    logger.info(f"Calling signup_client with the following data: {data}")
+    await signup_client(data)
     return JSONResponse(
         status_code=201, content={"message": "Customer created successfully"}
     )
