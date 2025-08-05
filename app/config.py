7e80fc96d4544200cea0f79da039835f5c63db0d
richardesp <espantaleon2001@gmail.com>
2025-08-06 00:14:17 +0200

    chore: define config variables

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+
+# App variables
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+LOG_PATH = os.getenv("LOG_PATH", "logs/app.log")
+
+# Database variables
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+DATABASE_URL = (
+    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
